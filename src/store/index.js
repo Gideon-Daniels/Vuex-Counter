4db310bb37b249979e34b92a670f85219f89b702
Gideon-Daniels <gideondaniels.dragoonix@gmail.com>
2021-11-11 13:12:16 +0200

    first commit

--- src/store/index.js
+++ src/store/index.js
@@ -1,11 +1,42 @@
 import { createStore } from 'vuex'
+import axios from 'axios'
 
 export default createStore({
   state: {
+    counter: 0
   },
+  // methods or functions that changes the states properties
+  // commit methods
   mutations: {
+    increaseCounter(state, randomNumber) {
+      state.counter += randomNumber
+    },
+      decreaseCounter(state, randomNumber) {
+     state.counter -= randomNumber
+    }
   },
+  // can have asynchronous code
+  // dispatch method
   actions: {
+    increaseCounter({ commit }){
+      // fetch data from api
+      axios('https://www.random.org/integers/?num=1&min=1&max=6&col=1&base=10&format=plain&rnd=new')
+      .then(response => {
+        // send random number as payload to mutation
+       commit('increaseCounter', response.data)
+      })
+    },
+    decreaseCounter({ commit }){
+      // fetch data from api
+      axios('https://www.random.org/integers/?num=1&min=1&max=6&col=1&base=10&format=plain&rnd=new')
+      .then(response => {
+        // send random number as payload to mutation
+       commit('decreaseCounter', response.data)
+      })
+    }
+  },
+  getters: {
+
   },
   modules: {
   }
